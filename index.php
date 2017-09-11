e20b7622083e689f24e27ad200d49284112336a5
prithvi1972 <31385310+prithvi1972@users.noreply.github.com>
2017-09-11 22:25:10 +0530

    Add files via upload

--- index.php
+++ index.php
@@ -1,15 +1,17 @@
 <?php
 require_once 'phpfunc.inc.php';
-if (isset($_GET['tech'])) {
+if (isset($_GET['technical'])) {
 	require_once 'tech.inc.php';
-} elseif (isset($_GET['culture'])) {
+} elseif (isset($_GET['cultural'])) {
 	require_once 'culture.inc.php';
-} elseif (isset($_GET['attract'])) {
+} elseif (isset($_GET['attraction'])) {
 	require_once 'attract.inc.php';
 } elseif (isset($_GET['contact'])) {
 	require_once 'contact.inc.php';
-} elseif (isset($_GET['sponsor'])) {
+} elseif (isset($_GET['sponsors'])) {
 	require_once 'sponsor.inc.php';
+} elseif (isset($_GET['home'])) {
+	require_once 'home.inc.php';
 } else {
 	require_once 'index.html';
 }
--- tech.inc.php
+++ tech.inc.php
@@ -4,122 +4,20 @@
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link href='http://fonts.googleapis.com/css?family=Open+Sans:400,300,700' rel='stylesheet' type='text/css'>
-	<link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet"> <!-- Bootstrap -->
+	<!-- Bootstrap -->
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+	<link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css?family=Capriola" rel="stylesheet">
 	<link href="css/card.css" rel="stylesheet">
 	<title>Saturnalia 2017</title>
-
-	<style type="text/css">
-		.card {
-			background-color: #eee;
-			padding: 0;
-			margin: 0;
-		}
-
-		.card-image-container {
-			padding: 0;
-			max-width: 300px;
-			background-image: url('img/p2.jpg');
-			overflow: hidden;
-		}
-
-		.card-content {
-			margin-right: 0;
-		}
-
-		.card-image {
-			padding: 0;
-			height: 100%;
-			width: auto;
-		}
-
-		.card-rules {
-			float: right;
-			cursor: pointer;
-		}
-
-		.card-name {
-			font-size: 2.2em;
-			line-height: 0.88;
-			border-bottom: 1px black;
-			border-bottom-style: dashed;
-			text-shadow:
-			2px 2px #eee,
-			2px -2px #eee,
-			-2px 2px #eee,
-			-2px -2px #eee;
-		}
-
-		.card-price {
-			position: absolute;
-			right: 10px;
-			top: 10px;
-			font-size: 1.1em;
-			text-transform: uppercase;
-			letter-spacing: 1.5pt;
-		}
-
-		.card-details {
-			list-style: none;
-			margin:0;
-			padding:0;
-		}
-	</style>
-
 </head>
 
 <body>
-
+	<div class="jumbotron"><h2>Technical</h2></div>
 	<div class="container" id="cards"></div>
-
 	<script src="js/jquery-2.1.4.js"></script>
-	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script><!--Bootstrap-->
-	<script type="text/javascript">
-		function toggle(i) {
-			$('#rules'+i).slideToggle();
-		}
-		var x = JSON.parse(<?php echo getJSON('tech'); ?>);
-		$(document).ready(function() {
-			for (var i = 0; i < x.length; i++) {
-
-				var $card = $('<div/>').addClass("card row");
-
-				$card.append(
-					$('<div />')
-					.addClass('card-image-container col-xs-4')
-					.append('<img>')
-						.attr('src', 'img/'+x[i].photo+'.jpg')
-						.addClass('card-image')
-				);
-
-				var $content = $('<div />').addClass('card-content col-xs-8');
-				$content.append($('<h4 />').addClass('card-name').text(x[i].title));
-				$content.append($('<p />').addClass('card-desc').text(x[i].desc));
-				$content.append($('<div />').addClass('card-price').text(x[i].prize));
-
-				var $desc = $('<ul />').addClass('card-details');
-				$desc.append($('<li />').addClass('card-date').text(x[i].date));
-				$desc.append($('<li />').addClass('card-venue').text(x[i].venue));
-				$desc.append($('<li />').addClass('card-time').text(x[i].time));
-				$content.append($desc);
-
-				var $link = $('<a />').addClass('card-rules-toggle').attr('onclick', 'toggle('+i+');').append($('<span />').addClass('fa fa-angle-down'));
-				$link.append('rules');
-				$content.append($link);
-
-				var $rules = $('<div />').addClass('card-rules').attr('id', 'rules'+i);
-
-				var $list = $('<ol />').addClass('card-rule-list');
-				for (var j = 0; j < x[i].rules.length; j++) {
-					$list.append($('<li />').text(x[i].rules[j].desc));
-				}
-				$rules.append($list);
-				$content.append($rules.hide());
-
-				$card.append($content);
-
-				$('#cards').append($card);
-			}
-		});
-	</script>
+	<script type="text/javascript">var x = JSON.parse(<?php echo getJSON('tech');?>);</script>
+	<script src="js/card.js"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
 </body>
 </html>
